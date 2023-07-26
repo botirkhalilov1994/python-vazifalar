195ae14f4c23690bfe4021a2b05009af9e26b153
botirkhalilov1994 <140585609+botirkhalilov1994@users.noreply.github.com>
2023-07-26 11:33:36 +0500

    Add files via upload

---
+++ vazifa.py
@@ -0,0 +1,33 @@
+# Amaliyot
+
+# ismlar = ['Elbek', 'Begzod', 'Jahongir', 'Ulug\'bek', 'Nodirxon']
+# for ism in ismlar:
+#     print(f"Salom {ism}")
+# print(f"Kod {len(ismlar)} marta takrorlandi.")
+
+# toq_sonlar = [x for x in range(10, 101) if x % 2 != 0]
+# for number in toq_sonlar:
+#     cube = number**3
+#     print(cube)
+
+# sonlar = list(range(11, 100, 2))
+# for son in sonlar:
+#     print(son**3)
+
+# kinolar = []
+# print("5 ta eng sevimli kinolaringizni kiriting: ")
+# for n in range(5):
+#     kinolar.append(input(f"{n+1}-sevimli kinoyingiz nomi: "))
+# print(kinolar)
+
+# n_people = int(input("Bugun necha kishi bn suhbat qildingiz?>>>"))
+# ismlar = []
+# for n in range(n_people):
+#     ismlar.append(input(f"{n+1}-suhbat qilgan odamingiz kim edi: "))
+# print(ismlar)
+
+# mehmonlar = int(input("Kecha uyingizga nechta odam keldi?>>>"))
+# ismlar = []
+# for n in range(mehmonlar):
+#     ismlar.append(input(f"{n+1}-kelgan mehmonnig ismini kiriting: "))
+# print(ismlar)
